c525d0d827b8a401aadbc9f7e9e9c0759bc0f685
Diego Medina <diegocab456@gmail.com>
2023-08-13 19:22:56 -0300

    uso del backend para mostrar la informacion en el home de heroes

--- src/app/heroes/pages/list-page/list-page.component.ts
+++ src/app/heroes/pages/list-page/list-page.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.servce';
 
 @Component({
   selector: 'app-list-page',
@@ -6,6 +9,13 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class ListPageComponent {
+export class ListPageComponent implements OnInit {
 
+  public heroes: Hero[] = [];
+
+  constructor( private heroesServices: HeroesService) {}
+
+  ngOnInit(): void {
+    this.heroesServices.getHeroes().subscribe( heroes => this.heroes = heroes );
+  }
 }
